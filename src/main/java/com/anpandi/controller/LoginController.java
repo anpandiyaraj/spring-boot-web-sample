69d7df657f94801d932f7ff8e8e2a9c8577a225e
anpandiyaraj <89447328+anpandiyaraj@users.noreply.github.com>
2024-08-23 16:04:06 +0530

    Add files via upload

--- src/main/java/com/anpandi/controller/LoginController.java
+++ src/main/java/com/anpandi/controller/LoginController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @Controller
 @RequestMapping("/")
@@ -31,5 +32,12 @@ public class LoginController {
             return "loginPage";
         }
     }
+    @GetMapping("/stream")
+    public SseEmitter streamData() {
+        SseEmitter emitter = new SseEmitter();
+        service.streamData(emitter);
+        return emitter;
+    }
+
 
 }
--- src/main/java/com/anpandi/service/LoginService.java
+++ src/main/java/com/anpandi/service/LoginService.java
@@ -1,6 +1,9 @@
 package com.anpandi.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.concurrent.Executors;
 
 @Service
 public class LoginService {
@@ -8,6 +11,20 @@ public class LoginService {
 	public boolean validateUser(String userid, String password) {
         return userid.equalsIgnoreCase("admin")
                 && password.equalsIgnoreCase("password");
+    }
+
+    public void streamData(SseEmitter emitter) {
+        Executors.newSingleThreadExecutor().execute(() -> {
+            try {
+                for (int i = 0; i < 10; i++) {
+                    emitter.send("Data " + i);
+                    Thread.sleep(1000);
+                }
+                emitter.complete();
+            } catch (Exception e) {
+                emitter.completeWithError(e);
+            }
+        });
     }
 
 }
